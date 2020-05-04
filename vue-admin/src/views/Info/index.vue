be53ad787dd01c055db18fae81080fa8366139da
dengchang <1332524093@qq.com>
2020-05-05 00:32:10 +0800

    在页面实现日期组件配置数据格式化、单条数据删除、批量数据删除、数据筛选

--- vue-admin/src/views/Info/index.vue
+++ vue-admin/src/views/Info/index.vue
@@ -28,6 +28,8 @@
             <el-date-picker
               v-model="date_value"
               type="datetimerange"
+              format="yyyy 年 MM 月 dd 日"
+              value-format="yyyy-MM-dd HH:mm:ss"
               align="right"
               start-placeholder="开始日期"
               end-placeholder="结束日期"
@@ -62,7 +64,11 @@
         ></el-input>
       </el-col>
       <el-col :span="2">
-        <el-button type="danger" size="medium" style="width:100%;"
+        <el-button
+          type="danger"
+          size="medium"
+          style="width:100%;"
+          @click="getList"
           >搜索</el-button
         >
       </el-col>
@@ -78,23 +84,35 @@
     </el-row>
     <div class="black-space-30"></div>
     <!-- 表格数据 -->
-    <el-table :data="table_data.item" border style="width:100%">
+    <el-table
+      :data="table_data.item"
+      v-loading="loadingData"
+      border
+      style="width:100%"
+      @selection-change="handleSelectionChange"
+    >
       <el-table-column type="selection" width="45"> </el-table-column>
       <el-table-column prop="title" label="标题" width="400"></el-table-column>
       <el-table-column
         prop="categoryId"
         label="类型"
         width="110"
+        :formatter="toCategory"
       ></el-table-column>
       <el-table-column
         prop="createDate"
         label="日期"
         width="180"
+        :formatter="toData"
       ></el-table-column>
       <el-table-column prop="user" label="管理员" width="100"></el-table-column>
       <el-table-column label="操作">
-        <template>
-          <el-button type="danger" size="mini" @click="deleteItem">
+        <template slot-scope="scope">
+          <el-button
+            type="danger"
+            size="mini"
+            @click="deleteItem(scope.row.id)"
+          >
             删除
           </el-button>
           <el-button type="success" size="mini" @click="dialog_info = true"
@@ -132,39 +150,40 @@
 <script>
 import DialogInfo from "./dialog/info";
 import { global } from "@/utils/global_V3.0.js";
-import { GetCategory, GetList } from "@/api/news";
+import { GetCategory, GetList, DeleteInfo } from "@/api/news";
 import { common } from "@/api/common";
+import { timestampToTime } from "@/utils/common";
 import { reactive, ref, onMounted, watch } from "@vue/composition-api";
 export default {
   name: "InfoIndex",
   components: {
-    DialogInfo
+    DialogInfo,
   },
   setup(props, { root }) {
     const { getInfoCategory, categoryItem } = common();
-    const { confirm } = global();
+    const { confirm, message } = global();
     const options = reactive({
-      category: []
+      category: [],
     });
     //搜索关键字
     const search_option = reactive([
       {
         value: "id",
-        label: "ID"
+        label: "ID",
       },
       {
         value: "title",
-        label: "标题"
-      }
+        label: "标题",
+      },
     ]);
     // 表格数据
     const table_data = reactive({
-      item: []
+      item: [],
     });
     // 页码
     const page = reactive({
       pageNumber: 1,
-      pageSize: 10
+      pageSize: 10,
     });
     // 信息弹窗标记
     const dialog_info = ref(false);
@@ -174,66 +193,124 @@ export default {
     const date_value = ref("");
     const search_keyWork = ref("");
     const total = ref(0);
+    const deleteInfoId = ref("");
+    const loadingData = ref(false);
     // vue2.0 methods
     // 需要显示多少条数据
-    const handleSizeChange = val => {
+    const handleSizeChange = (val) => {
       page.pageSize = val;
       getList();
     };
     // 下一页数据
-    const handleCurrentChange = val => {
+    const handleCurrentChange = (val) => {
       page.pageNumber = val;
       getList();
     };
     // 是否删除信息确认弹窗
-    const deleteItem = () => {
+    const deleteItem = (id) => {
+      deleteInfoId.value = [id];
       confirm({
         content: "确定删除当前信息，确认后将无法恢复！！",
         tip: "警告",
         fn: confirmDelete,
-        id: 1111111
       });
-      /* root.confirm({
-        content: "确定删除当前信息，确认后将无法恢复！！",
-        tip: "警告",
-        fn: confirmDelete,
-        id: 1111111
-      }); */
     };
-
+    // 删除所有选中的数据
     const deleteAll = () => {
+      if (!deleteInfoId.value || deleteInfoId.length == 0) {
+        message({
+          type: "error",
+          message: "请选择要删除的数据！！",
+        });
+        return false;
+      }
       confirm({
         content: "确定删除选择的数据，确认后将无法恢复！！",
         fn: confirmDelete,
-        id: 2222222
       });
     };
-    const getList = () => {
+    // 删除数据处理函数
+    const confirmDelete = (value) => {
+      DeleteInfo({ id: deleteInfoId.value }).then((respones) => {
+        message({
+          type: "success",
+          message: respones.data.message,
+        });
+        deleteInfoId.value = "";
+        getList();
+      });
+    };
+    // 处理选中函数
+    const handleSelectionChange = (val) => {
+      let id = val.map((item) => item.id);
+      deleteInfoId.value = id;
+    };
+    // 搜索
+  /*   const search = () => {
+      let resquestData = formatData();
+      getList();
+    }; */
+    // 搜索条件处理
+    const formatData = () => {
       let resquestData = {
+        pageNumber: page.pageNumber,
+        pageSize: page.pageSize,
+      };
+      // 分类id
+      if (category_value.value) {
+        resquestData.categoryId = category_value.value;
+      }
+      // 日期
+      if (date_value.value.length > 0) {
+        requestData.startTime = date_value.value[0];
+        requestData.endTime = date_value.value[1];
+      }
+      // 关键字
+      if (search_keyWork.value) {
+        resquestData[search_key.value] = search_keyWork.value;
+      }
+
+      return resquestData;
+    };
+
+    const getList = () => {
+      /* let resquestData = {
         categoryId: "",
         startTime: "",
         endTime: "",
         title: "",
         id: "",
         pageNumber: page.pageNumber,
-        pageSize: page.pageSize
-      };
+        pageSize: page.pageSize,
+      }; */
+      let resquestData = formatData();
+      loadingData.value = true;
       GetList(resquestData)
-        .then(respones => {
+        .then((respones) => {
           let data = respones.data.data;
           console.log(data);
           // 表格数据
           table_data.item = data.data;
           // 页码数量
           total.value = data.total;
+          loadingData.value = false;
         })
-        .catch(error => {
+        .catch((error) => {
+          loadingData.value = false;
           console.log(error);
         });
     };
-
-    const confirmDelete = value => {
-      console.log(value);
+    // 格式化时间
+    const toData = (row, column, cellValue, index) => {
+      return timestampToTime(row.createDate);
+    };
+    // 匹配分类
+    const toCategory = (row) => {
+      let categoryId = row.categoryId;
+      let categoryData = options.category.filter(
+        (item) => item.id == categoryId
+      )[0];
+      return categoryData.category_name;
     };
     // 生命周期
     // 挂载完成时执行，（页面DOM元素完成时，实际完成）
@@ -241,7 +318,7 @@ export default {
       // vue3.0
       // getInfoCategory();
       // vuex
-      root.$store.dispatch("common/getInfoCategory").then(res => {
+      root.$store.dispatch("common/getInfoCategory").then((res) => {
         options.category = res;
       });
       getList();
@@ -263,13 +340,18 @@ export default {
       date_value,
       search_keyWork,
       total,
+      loadingData,
       // methods
       handleSizeChange,
       handleCurrentChange,
       deleteItem,
-      deleteAll
+      deleteAll,
+      toData,
+      toCategory,
+      handleSelectionChange,
+      getList,
     };
-  }
+  },
 };
 </script>
 <style lang="scss" scoped>
