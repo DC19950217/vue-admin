28d1eba491c90216db87b0e5646a2fe5e0b8b761
dengchang <1332524093@qq.com>
2020-06-02 16:39:37 +0800

    用户管理页完成header

--- vue-admin/src/views/User/index.vue
+++ vue-admin/src/views/User/index.vue
@@ -1,8 +1,53 @@
 <template>
-  <div>user</div>
+  <div>
+    <el-row>
+      <el-col :span="20">
+        <div class="label-wrap">
+          <label for="">关键字：</label>
+          <div class="warp-content">
+            <el-row :gutter="16">
+              <el-col :span="3">
+                <SelectVue :config="data.configOption" />
+              </el-col>
+              <el-col :span="5">
+                <el-input placeholder="请输入搜索的关键字"></el-input>
+              </el-col>
+              <el-col :span="15">
+                <el-button type="danger">搜索</el-button>
+              </el-col>
+            </el-row>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="4"
+        ><el-button type="danger" class="pull-right"
+          >添加用户</el-button
+        ></el-col
+      >
+    </el-row>
+  </div>
 </template>
 <script>
-export default {};
+import { reactive, ref, onMounted, watch } from "@vue/composition-api";
+import SelectVue from "@/components/Select";
+export default {
+  name: "userIndex",
+  components: { SelectVue },
+  setup(props) {
+    const data = reactive({
+      configOption: {
+        init: ["name", "phone", "email"]
+      }
+    });
+    return {
+      data
+    };
+  }
+};
 </script>
 <style lang="scss" scoped>
+@import "../../styles/config.scss";
+.label-wrap {
+  @include labelDom(left, 60, 40);
+}
 </style>
