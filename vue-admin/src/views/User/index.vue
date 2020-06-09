41f7e95fb0a8e71ee56497f45dcbc419e857ce3e
dengchang <1332524093@qq.com>
2020-06-09 17:03:24 +0800

    使用封装的table组件

--- vue-admin/src/views/User/index.vue
+++ vue-admin/src/views/User/index.vue
@@ -25,22 +25,77 @@
         ></el-col
       >
     </el-row>
+    <div class="black-space-30"></div>
+    <TableVue :config="data.configTable">
+      <template v-slot:status="slotData">
+        <el-switch
+          v-model="slotData.data.role"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+        >
+        </el-switch>
+      </template>
+      <template v-slot:operation="slotData">
+        <el-button type="danger" size="small" @click="operation(slotData.data)"
+          >删除</el-button
+        >
+        <el-button type="success" size="small" @click="operation(slotData.data)"
+          >编辑</el-button
+        >
+      </template>
+    </TableVue>
   </div>
 </template>
 <script>
 import { reactive, ref, onMounted, watch } from "@vue/composition-api";
 import SelectVue from "@/components/Select";
+import TableVue from "@/components/Table";
 export default {
   name: "userIndex",
-  components: { SelectVue },
+  components: { SelectVue, TableVue },
   setup(props) {
     const data = reactive({
       configOption: {
         init: ["name", "phone", "email"]
+      },
+      configTable: {
+        selection: true,
+        tHead: [
+          { label: "邮箱/用户名", field: "email", width: 180 },
+          { label: "真实姓名", field: "name", width: 100 },
+          { label: "手机号", field: "phone", width: 110 },
+          { label: "地区", field: "address" },
+          { label: "角色", field: "role", width: 80 },
+          {
+            label: "禁启用状态",
+            field: "status",
+            columnType: "slot",
+            slotName: "status",
+            width: 100
+          },
+          {
+            label: "操作",
+            columnType: "slot",
+            slotName: "operation"
+          }
+        ],
+        // 请求接口URL
+        requestData: {
+          url: "getUserList",
+          method: "post",
+          data: {
+            pageNumber: 1,
+            pageSize: 10
+          }
+        }
       }
     });
+    const operation = params => {
+      console.log(params);
+    };
     return {
-      data
+      data,
+      operation
     };
   }
 };
