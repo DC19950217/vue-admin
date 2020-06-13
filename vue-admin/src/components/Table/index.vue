2776b37f3e90651c13750d9314522516ca687575
dengchang <1332524093@qq.com>
2020-06-13 10:24:56 +0800

    在table组件添加翻页功能

--- vue-admin/src/components/Table/index.vue
+++ vue-admin/src/components/Table/index.vue
@@ -1,41 +1,55 @@
 <template>
-  <el-table
-    :data="data.tableData"
-    border
-    style="width: 100%"
-    @selection-change="handleSelectionChange"
-  >
-    <!-- 多选框 -->
-    <el-table-column
-      v-if="data.tableConfig.selection"
-      type="selection"
-      width="55"
+  <div>
+    <el-table
+      :data="data.tableData"
+      border
+      style="width: 100%"
+      @selection-change="handleSelectionChange"
     >
-    </el-table-column>
+      <!-- 多选框 -->
+      <el-table-column
+        v-if="data.tableConfig.selection"
+        type="selection"
+        width="55"
+      >
+      </el-table-column>
 
-    <template v-for="item in data.tableConfig.tHead">
-      <el-table-column
-        :key="item.field"
-        :prop="item.field"
-        :label="item.label"
-        :width="item.width"
-        v-if="item.columnType === 'slot'"
-      >
-        <template slot-scope="scope">
-          <slot :name="item.slotName" :data="scope.row"></slot>
-        </template>
-      </el-table-column>
-      <!-- 文本数据渲染 -->
-      <el-table-column
-        v-else
-        :key="item.field"
-        :prop="item.field"
-        :label="item.label"
-        :width="item.width"
-      >
-      </el-table-column>
-    </template>
-  </el-table>
+      <template v-for="item in data.tableConfig.tHead">
+        <el-table-column
+          :key="item.field"
+          :prop="item.field"
+          :label="item.label"
+          :width="item.width"
+          v-if="item.columnType === 'slot'"
+        >
+          <template slot-scope="scope">
+            <slot :name="item.slotName" :data="scope.row"></slot>
+          </template>
+        </el-table-column>
+        <!-- 文本数据渲染 -->
+        <el-table-column
+          v-else
+          :key="item.field"
+          :prop="item.field"
+          :label="item.label"
+          :width="item.width"
+        >
+        </el-table-column>
+      </template>
+    </el-table>
+    <el-pagination
+      v-if="data.tableConfig.paginationShow"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageData.currentPage"
+      :page-sizes="pageData.pageSizes"
+      :page-size="pageData.pageSize"
+      :layout="data.tableConfig.paginationLayout"
+      :total="pageData.total"
+      background
+    >
+    </el-pagination>
+  </div>
 </template>
 <script>
 import {
@@ -46,6 +60,7 @@ import {
   onBeforeMount
 } from "@vue/composition-api";
 import { loadData } from "./tableLoadData";
+import { paginationHook } from "./paginationHook";
 export default {
   name: "tableVue",
   props: {
@@ -56,29 +71,22 @@ export default {
   },
   setup(props, { root }) {
     const { tableData, tableLoadData } = loadData();
+    const {
+      pageData,
+      handleSizeChange,
+      handleCurrentChange,
+      totalCount
+    } = paginationHook();
     const data = reactive({
       tableConfig: {
         selection: true,
         recordCheckbox: false,
         requestData: {},
-        tHead: []
+        tHead: [],
+        paginationLayout: "total, sizes, prev, pager, next, jumper",
+        paginationShow: true
       },
-      tableData: [
-        {
-          email: "15218298951@163.com",
-          name: "王小虎",
-          phone: "13358957147",
-          address: "上海市普陀区金沙江路 1518 弄",
-          role: "超管"
-        },
-        {
-          email: "15218298951@163.com",
-          name: "王小虎",
-          phone: "13358957147",
-          address: "上海市普陀区金沙江路 1518 弄",
-          role: "超管"
-        }
-      ]
+      tableData: []
     });
     let handleSelectionChange = () => {};
 
@@ -91,9 +99,30 @@ export default {
         }
       }
     };
-    watch(
+    /*  watch(
       () => tableData.item,
       (newValue, oldValue) => (data.tableData = newValue)
+    ); */
+    // 数据渲染
+    watch(
+      [() => tableData.item, () => tableData.total],
+      ([tableData, total]) => {
+        data.tableData = tableData;
+        totalCount(total);
+      }
+    );
+    watch(
+      [() => pageData.currentPage, () => pageData.pageSize],
+      ([currentPage, pageSize]) => {
+        let requestData = data.tableConfig.requestData;
+        if (requestData.data) {
+          console.log(requestData);
+          requestData.data.pageNumber = currentPage;
+          requestData.data.pageSize = pageSize;
+
+          tableLoadData(data.tableConfig.requestData);
+        }
+      }
     );
     onBeforeMount(() => {
       initTableConfig();
@@ -101,7 +130,11 @@ export default {
     });
     return {
       data,
-      handleSelectionChange
+      handleSelectionChange,
+      pageData,
+      handleSizeChange,
+      handleCurrentChange,
+      totalCount
     };
   }
 };
