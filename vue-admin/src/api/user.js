1290067b268ca3f22f1018e674c847e7b030cf79
dengchang <1332524093@qq.com>
2020-06-27 20:01:07 +0800

    新增用户添加接口和用户删除接口

--- vue-admin/src/api/user.js
+++ vue-admin/src/api/user.js
@@ -1,8 +1,25 @@
 import serveice from '@/utils/request';
+// 获取用户列表
 export function GetRole(data = {}) {
   return serveice.request({
     method: "post",
     url: "/role/",
+    data: data
+  })
+}
+// 添加用户
+export function UserAdd(data = {}) {
+  return serveice.request({
+    method: "post",
+    url: "/user/add/",
+    data: data
+  })
+}
+// 删除用户
+export function UserDel(data) {
+  return serveice.request({
+    method: "post",
+    url: "/user/delete/",
     data: data
   })
 }
