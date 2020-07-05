d4763033b7f3c15b3a5c5dde50c1cb9f55d0dc1e
dengchang <1332524093@qq.com>
2020-07-05 19:59:07 +0800

    新增数据验证（用户名、密码、角色），接收父组件传递用来处理用户编辑的数据，把用户添加和用户编辑封装成函数来调用

--- vue-admin/src/views/User/dialog/add.vue
+++ vue-admin/src/views/User/dialog/add.vue
@@ -6,7 +6,7 @@
     width="580"
     @opened="openDialog"
   >
-    <el-form :model="data.form" ref="addInfoForm">
+    <el-form :model="data.form" :rules="data.rules" ref="addInfoForm">
       <el-form-item
         label="用户名："
         :label-width="data.formLabelWidth"
@@ -80,7 +80,10 @@
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button @click="close">取消</el-button>
-      <el-button type="danger" :loading="data.submitLoading" @click="submit"
+      <el-button
+        type="danger"
+        :loading="data.submitLoading"
+        @click="submit('addInfoForm')"
         >确定</el-button
       >
     </div>
@@ -98,31 +101,73 @@ import {
   onBeforeMount
 } from "@vue/composition-api";
 import CityPickerVue from "@/components/CityPicker";
-import { GetRole, UserAdd } from "@/api/user";
+import { GetRole, UserAdd, UserEdit } from "@/api/user";
 import { global } from "@/utils/global_V3.0.js";
+import { stripscript, validateEmail, validatePass } from "@/utils/validate";
 export default {
   name: "dialogInfo",
   props: {
     flag: {
       type: Boolean,
       default: false
+    },
+    editData: {
+      type: Object,
+      default: () => {}
     }
   },
   components: { CityPickerVue },
   setup(props, { emit, root, refs }) {
+    // 验证用户名
+    let validateUsername = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入用户名！"));
+      } else if (validateEmail(value)) {
+        callback(new Error("用户名格式有误！"));
+      } else {
+        callback();
+      }
+    };
+
+    // 验证密码
+    let validatePassword = (rule, value, callback) => {
+      if (data.form.id && !value) {
+        callback();
+      }
+      if ((data.form.id && value) || !data.form.id) {
+        // 过滤后的数据
+        if (value) {
+          data.form.password = stripscript(value);
+          value = data.form.password;
+        }
+
+        if (value === "") {
+          callback(new Error("请输入密码"));
+        } else if (validatePass(value)) {
+          callback(new Error("密码为6至20位数字+字母"));
+        } else {
+          callback();
+        }
+      }
+    };
     const data = reactive({
       dialog_info_flag: false, //弹窗标记
       formLabelWidth: "90px",
       submitLoading: false, //按钮加载
       form: {
         //表单数据
-        username: "",
+        username: "1332524093@qq.com",
         password: "",
-        truename: "",
-        phone: "",
+        truename: "dc",
+        phone: "15218298951",
         region: "",
         status: "1",
         role: []
+      },
+      rules: {
+        username: [{ validator: validateUsername, trigger: "blur" }],
+        password: [{ validator: validatePassword, trigger: "blur" }],
+        role: [{ required: true, message: "请选择角色", trigger: "change" }]
       },
 
       // 城市数据
@@ -139,48 +184,81 @@ export default {
     };
     // 窗口打开，动画结束时
     const openDialog = () => {
+      // 角色请求
       getRole();
+      // 初始值处理
+      let editData = props.editData;
+      if (editData && editData.id) {
+        editData.role = editData.role.split(",");
+      } else {
+        data.form.id && delete data.form.id;
+      }
+      for (let key in editData) {
+        data.form[key] = editData.id ? editData[key] : "";
+      }
     };
     // 添加信息
-    const submit = () => {
-      if (!data.form.username) {
-        message({
-          type: "error",
-          message: "用户名不能为空！！"
-        });
-        return false;
-      }
-      if (!data.form.password) {
-        message({
-          type: "error",
-          message: "密码不能为空！！"
-        });
-        return false;
-      }
-      if (!data.form.role.length === 0) {
-        message({
-          type: "error",
-          message: "请选择角色类型！！"
-        });
-        return false;
-      }
-      // 简单深拷贝 JSON.parse(JSON.stringify(data.form))
-      // 浅拷贝
-      let requestData = Object.assign({}, data.form);
-      requestData.role = requestData.role.join();
-      requestData.password = sha1(requestData.password);
-      requestData.region = JSON.stringify(data.cityPickerData);
-      submitLoadingFn(true);
+    const submit = formName => {
+      refs[formName].validate(valid => {
+        if (valid) {
+          // 简单深拷贝 JSON.parse(JSON.stringify(data.form))
+          // 浅拷贝
+          let requestData = Object.assign({}, data.form);
+          requestData.role = requestData.role.join();
+
+          requestData.region = JSON.stringify(data.cityPickerData);
+          submitLoadingFn(true);
+
+          if (requestData.id) {
+            if (requestData.password) {
+              requestData.password = sha1(requestData.password);
+            } else {
+              delete requestData.password;
+            }
+            userEditFn(requestData);
+          } else {
+            requestData.password = sha1(requestData.password);
+            userAddFn(requestData);
+          }
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    };
+    // 封装用户添加函数
+    const userAddFn = requestData => {
       UserAdd(requestData)
         .then(response => {
           message({
             type: "success",
             message: response.data.message
           });
-          resetForm();
           submitLoadingFn(false);
+          close();
+          // 回传父组件刷新数据
+          emit("refreshTableData");
         })
-        .catch(error => {});
+        .catch(error => {
+          submitLoadingFn(false);
+        });
+    };
+    // 封装用户编辑函数
+    const userEditFn = requestData => {
+      UserEdit(requestData)
+        .then(response => {
+          message({
+            type: "success",
+            message: response.data.message
+          });
+          submitLoadingFn(false);
+          close();
+          // 回传父组件刷新数据
+          emit("refreshTableData");
+        })
+        .catch(error => {
+          submitLoadingFn(false);
+        });
     };
     //清空表单
     const resetForm = () => {
