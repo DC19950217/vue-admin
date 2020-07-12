da200f6ca634d623a34cc1648bed1e08a91b52c6
dengchang <1332524093@qq.com>
2020-07-12 17:08:51 +0800

    利用路由守卫处理动态路由

--- vue-admin/src/router/premit.js
+++ vue-admin/src/router/premit.js
@@ -1,6 +1,9 @@
 import router from "./index";
 import store from "../store/index";
-import {removeToken,removeUserName} from "@/utils/app";
+import {
+    removeToken,
+    removeUserName
+} from "@/utils/app";
 
 import {
     getToken
@@ -8,15 +11,34 @@ import {
 const whiteRouter = ['/login'];
 router.beforeEach((to, from, next) => {
     if (getToken()) {
-       if(to.path==="/login"){
-        removeToken();
-        removeUserName();
-        store.commit("SET_TOKEN","")
-        store.commit("SET_USERNAME","")
-        next();
-       }else{
-        next();
-       }
+        if (to.path === "/login") {
+            removeToken();
+            removeUserName();
+            store.commit("SET_TOKEN", "")
+            store.commit("SET_USERNAME", "")
+            next();
+        } else {
+            if (store.getters['app/roles'].length === 0) {
+                store.dispatch('permission/getRoles').then(response => {
+                    let role = response.role
+                    store.commit('app/SET_ROLES', role)
+                    store.dispatch('permission/createRouetr', role).then(response => {
+                        let addRouters = store.getters['permission/addRouters'];
+                        let allRouters = store.getters['permission/allRouters'];
+                        // 路由更新
+                        router.options.routes = allRouters;
+                        // 添加动态路由
+                        router.addRoutes(addRouters)
+                        next({
+                            ...to,
+                            replace: true
+                        });
+                    })
+                })
+            } else {
+                next();
+            }
+        }
         console.log("存在");
     } else {
         if (whiteRouter.indexOf(to.path) !== -1) {
@@ -25,5 +47,5 @@ router.beforeEach((to, from, next) => {
             next('/login')
         }
     }
-    
+
 })
