dffd93d0c832a701f4aabd126bb7299d00b8e197
dengchang <1332524093@qq.com>
2020-07-15 23:53:20 +0800

    引入两种不同处理按钮权限的方法

--- vue-admin/src/main.js
+++ vue-admin/src/main.js
@@ -8,12 +8,21 @@ import "./plugins/composition-api";
 // import global from '@/utils/global.js'
 // 自定义全局组件
 import "./icons/index.js";
+// 引入按钮权限
+import {
+  buttonPermission
+} from './utils/buttonPermission'
+
+
 // 引入路由守卫
 import "./router/premit";
 
 import router from "./router";
 import store from "./store";
 
+Vue.prototype.btnPerm = buttonPermission
+// 引入自定义指令权限按钮
+import './utils/buttonPerm'
 // Vue.use(global);
 
 Vue.config.productionTip = false;
